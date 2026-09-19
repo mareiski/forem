5dcd5c57074420c344de857ba10aa65195672348
Marco <marco.reisinger@t-online.de>
2026-09-19 22:47:17 +0200

    try again

--- app/controllers/api/v0/firebase_auth_controller.rb
+++ app/controllers/api/v0/firebase_auth_controller.rb
@@ -38,8 +38,8 @@ module Api
       private
 
       def configure_cross_origin_session
-        request.session_options[:same_site] = :none
-        request.session_options[:secure] = request.origin.to_s.start_with?("https://")
+        request.session_options[:same_site] = :lax
+        request.session_options[:secure] = true if request.origin.to_s.start_with?("https://")
       end
 
       def firebase_auth_payload(claims)
